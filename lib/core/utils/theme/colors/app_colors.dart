f44d980e39e23a1ee3ace6f0fdc149ed5fb6d3c5
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-11-01 12:43:29 +0100

    create app light colors

---
+++ lib/core/utils/theme/colors/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const kPrimaryColor = Colors.black;
+  static const kSecondaryColor = Colors.white;
+  static const kMainGrayColor = Color(0xff71717A);
+  static const kSecondaryGrayColor = Color(0xffD4D4D8);
+}
---
+++ lib/core/utils/theme/colors/light_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio_ayhem_hamdi/core/utils/theme/colors/app_colors.dart';
+
+abstract class AppLightColors {
+  static Color primaryColor = AppColors.kPrimaryColor;
+  static Color onPrimaryColor = AppColors.kSecondaryColor;
+  static Color secondaryColor = AppColors.kSecondaryColor;
+  static Color onSecondaryColor = AppColors.kPrimaryColor;
+  static Color thirdColor = AppColors.kMainGrayColor;
+  static Color surfaceColor = AppColors.kPrimaryColor;
+  static Color onSurfaceColor = AppColors.kSecondaryGrayColor;
+}
