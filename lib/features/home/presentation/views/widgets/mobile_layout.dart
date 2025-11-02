cd24b640855474ffa3a58d435a6072b5117487c4
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-11-02 22:19:42 +0100

    Create Mobile Layout Banner

--- lib/features/home/presentation/views/widgets/mobile_layout.dart
+++ lib/features/home/presentation/views/widgets/mobile_layout.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:portfolio_ayhem_hamdi/core/utils/assets/app_assets.dart';
 
 class MobileLayout extends StatelessWidget {
   const MobileLayout({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Container());
+    return SafeArea(
+      child: CustomScrollView(
+        slivers: [SliverGap(40), MobileBanner(), const SliverGap(48)],
+      ),
+    );
+  }
+}
+
+class MobileBanner extends StatelessWidget {
+  const MobileBanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final double width = MediaQuery.sizeOf(context).width;
+    return SliverToBoxAdapter(
+      child: AspectRatio(
+        aspectRatio: width / 450,
+        child: SvgPicture.asset(AppAssets.mobileBanner),
+      ),
+    );
   }
 }
