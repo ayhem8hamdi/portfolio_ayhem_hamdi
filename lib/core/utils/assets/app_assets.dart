6065149751b124eba770dfdfdfa8b0a112c59ca8
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-11-02 14:19:28 +0100

    create app assets class

---
+++ lib/core/utils/assets/app_assets.dart
@@ -0,0 +1,32 @@
+abstract class AppAssets {
+  static const String _imagesBasePath = 'assets/images/';
+  static const String _iconBasePath = 'assets/icons/';
+  //images
+  static const String aboutMe = '${_imagesBasePath}about_me.svg';
+  static const String android = '${_imagesBasePath}android.svg';
+  static const String banner = '${_imagesBasePath}Banner.png';
+  static const String cubit = '${_imagesBasePath}cubit.svg';
+  static const String dart = '${_imagesBasePath}dart.svg';
+  static const String expressJs = '${_imagesBasePath}expressjs.svg';
+  static const String firebase = '${_imagesBasePath}firebase.svg';
+  static const String flutter = '${_imagesBasePath}flutter.svg';
+  static const String git = '${_imagesBasePath}git.svg';
+  static const String java = '${_imagesBasePath}java.svg';
+  static const String logo = '${_imagesBasePath}logo.svg';
+  static const String mobileAboutMe = '${_imagesBasePath}mobile_about_me.svg';
+  static const String mobileBanner = '${_imagesBasePath}mobile_banner.svg';
+  static const String mongoDb = '${_imagesBasePath}mongodb.svg';
+  static const String nodeJs = '${_imagesBasePath}nodejs.svg';
+  static const String supabase = '${_imagesBasePath}supabase-icon.svg';
+  // icons
+  static const String download = '${_iconBasePath}download.svg';
+  static const String facebook = '${_iconBasePath}fb.svg';
+  static const String github = '${_iconBasePath}github.svg';
+  static const String instagram = '${_iconBasePath}insta.svg';
+  static const String linkedIn = '${_iconBasePath}linked_in.svg';
+  static const String quote = '${_iconBasePath}quote.svg';
+  static const String readMore = '${_iconBasePath}Read_More.svg';
+  static const String redit = '${_iconBasePath}redit.svg';
+  static const String twitter = '${_iconBasePath}twitter.svg';
+  static const String youtube = '${_iconBasePath}youtube.svg';
+}
