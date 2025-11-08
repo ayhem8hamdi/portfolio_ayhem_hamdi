a744ae9815ac7b5ce3fe422bbcc8fc5ec6c9d80c
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-11-08 13:08:36 +0100

    Done Dependency injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:portfolio_ayhem_hamdi/core/dependency_injection.dart';
 import 'package:portfolio_ayhem_hamdi/core/utils/theme/app_theme.dart';
 import 'package:portfolio_ayhem_hamdi/features/home/presentation/views/home_screen.dart';
+import 'package:portfolio_ayhem_hamdi/features/home/presentation/manager/email_send_cubit/email_send_cubit.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupDI();
   runApp(
     DevicePreview(
       enabled: false,
@@ -17,13 +22,16 @@ class PortfolioAyhemHamdi extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      useInheritedMediaQuery: true,
-      locale: DevicePreview.locale(context),
-      builder: DevicePreview.appBuilder,
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      home: const HomeScreen(),
+    return BlocProvider(
+      create: (_) => getIt<EmailSendCubit>(),
+      child: MaterialApp(
+        useInheritedMediaQuery: true,
+        locale: DevicePreview.locale(context),
+        builder: DevicePreview.appBuilder,
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+        home: const HomeScreen(),
+      ),
     );
   }
 }
